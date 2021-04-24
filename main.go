8450973ef0d106cc446d63f88253f0be195d6324
Martin <persky03@gmail.com>
2021-04-24 11:09:33 +0200

    Basic go routes, file structure and comments

--- main.go
+++ main.go
@@ -5,8 +5,16 @@ import "github.com/gofiber/fiber/v2"
 func main() {
 	app := fiber.New()
 
+	// Homepage
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World 👋!")
+	})
+	// Static resources like CSS, JS, IMG
+	app.Static("/", "./public")
+
+	// Serve HTML file
+	app.Get("/default", func(c *fiber.Ctx) error {
+		return c.SendFile("./templates/default.html")
 	})
 
 	app.Listen(":3000")
